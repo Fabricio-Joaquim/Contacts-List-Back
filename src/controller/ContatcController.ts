a30318eae7f57c5eb3f5e6be2fd4dc73923a4674
Fabricio Joaquim <fabriciojoaquim25@hotmail.com>
2022-04-22 12:05:33 -0300

    tratamento de erros

--- src/controller/ContatcController.ts
+++ src/controller/ContatcController.ts
@@ -1,22 +1,19 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { Contact } from '../entity/Contact';
 import {AppDataSource} from '../connection';
 
 const userRepository = AppDataSource.getRepository(Contact);
 
-
-export const save = async (request: Request, response: Response, next: NextFunction) => {
-	const register = await userRepository.save(request.body);
-        
-	console.log(register);
+export const save = async (request: Request, response: Response) => {
+	const registerCreate = userRepository.create(request.body);
+	const register = await userRepository.save(registerCreate);
 	return response.json(register);
 };
     
-export const update = async (request: Request, response: Response, next: NextFunction) => {
+export const update = async (request: Request, response: Response) => {
 	const {id} = request.params;
 
 	const update = await userRepository.update(id,request.body);
-	console.log(id,update);
 	if(update.affected===1){
 		const findOne = await userRepository.findOneBy({id});
 		return response.json(findOne);
@@ -24,7 +21,7 @@ export const update = async (request: Request, response: Response, next: NextFun
 	return response.json({message: 'Não foi possível atualizar'});
 };
 
-export  const remove = async(request: Request, response: Response, next: NextFunction)=> {
+export  const remove = async(request: Request, response: Response)=> {
 	const {id} = request.params;
 	const userToRemove = await userRepository.findOneBy({ id });
 	let result;
@@ -34,7 +31,7 @@ export  const remove = async(request: Request, response: Response, next: NextFun
 	return response.json(result);
 };
 
-export const all = async (request: Request, response: Response, next: NextFunction) => {
+export const all = async (request: Request, response: Response) => {
 	const getAll = await userRepository.find();
 	console.log(getAll);
 	return response.json(getAll);
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { User } from '../entity/User';
 import {AppDataSource} from '../connection';
 import bcrypt from 'bcryptjs';
@@ -6,68 +6,108 @@ import jwt from 'jsonwebtoken';
 
 const userRepository = AppDataSource.getRepository(User);
 
-export const all = async (request: Request, response: Response, next: NextFunction) => {
-	const getAll = await userRepository.find();
-	return response.json(getAll);
+export const all = async (request: Request, response: Response) => {
+	try {
+		const getAll = await userRepository.find();
+		return response.json(getAll);
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
+	}
 };
 
-export const one = async(request: Request, response: Response, next: NextFunction) => {
-	const {id} = request.params;
-	const findOne = await userRepository.findOneBy({id});
-	if (!findOne) {
-		return response.status(404).json({message: 'User not found'});
-	}
-	return response.json(findOne);
-};
-export const login = async(request: Request, response: Response, next: NextFunction) => {
-	const {email, password} = request.body;
-	if (!email || !password) {
-		return response.status(400).json({message: 'Email and password are required'});
-	}
-	const findOne = await userRepository.findOne({where:{email}});
-	if(!findOne){
-		return response.status(404).json({message: 'User not found'});
-	}
-	const isPasswordMatch = bcrypt.compareSync(password, findOne.password);
-
-	if(!isPasswordMatch){
-		return response.status(401).json({message: 'Password not match'});
-	}
-	console.log(process.env.JWT_SECRET);
-	const token = jwt.sign({id: findOne.id}, process.env.JWT_SECRET || '11', {expiresIn: '1d'});
-
-	return response.json({
-		token,
-		email: findOne.email,
-		id: findOne.id
-	});
-
-};
-
-export const save = async (request: Request, response: Response, next: NextFunction) => {
-	const data = request.body;
-	const entity = userRepository.create(data);
-	await userRepository.save(entity);
-	
-	return response.json(entity);
-};
-export const update = async (request: Request, response: Response, next: NextFunction) => {
-	const {id} = request.params;
-	const update = await userRepository.update(id,request.body);
-	if(update.affected===1){
+export const one = async(request: Request, response: Response) => {
+	try {
+		const {id} = request.params;
 		const findOne = await userRepository.findOneBy({id});
+		if (!findOne) {
+			return response.status(404).json({message: 'User not found'});
+		}
 		return response.json(findOne);
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
 	}
-	return response.json({message: 'Não foi possível atualizar'});
+};
+export const login = async(request: Request, response: Response) => {
+	try {
+		const {email, password} = request.body;
+		if (!email || !password) {
+			return response.status(400).json({message: 'Email and password are required'});
+		}
+		const findOne = await userRepository.findOne({where:{email}});
+		if(!findOne){
+			return response.status(404).json({message: 'User not found'});
+		}
+		const isPasswordMatch = bcrypt.compareSync(password, findOne.password);
+	
+		if(!isPasswordMatch){
+			return response.status(401).json({message: 'Password not match'});
+		}
+		console.log(process.env.JWT_SECRET);
+		const token = jwt.sign({id: findOne.id}, process.env.JWT_SECRET || '11', {expiresIn: '1d'});
+	
+		return response.json({
+			token,
+			email: findOne.email,
+			id: findOne.id
+		});
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
+	}
+
 };
 
-export  const remove = async(request: Request, response: Response, next: NextFunction)=> {
-	const {id} = request.params;
-	const userToRemove = await userRepository.findOneBy({ id });
-	let result;
-	if(userToRemove)
-		result = await userRepository.remove(userToRemove);
+export const save = async (request: Request, response: Response) => {
+	try {
+		const {email} = request.body;
+		const findEmail = await userRepository.findOne({where:{email}});
+		if(findEmail){
+			return response.status(400).json({message: 'Email already exists'});
+		}
+		const data = request.body;
+		const entity = userRepository.create(data);
+		await userRepository.save(entity);
+		return response.json(entity);
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
+	}
+};
+export const update = async (request: Request, response: Response) => {
+	try {	
+		const {id} = request.params;
+		const update = await userRepository.update(id,request.body);
+		if(update.affected===1){
+			const findOne = await userRepository.findOneBy({id});
+			return response.json(findOne);
+		}
+		return response.json({message: 'Não foi possível atualizar'});
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
+	}
+};
 
-	return response.json(result);
+export  const remove = async(request: Request, response: Response)=> {
+	try {
+		const {id} = request.params;
+		const userToRemove = await userRepository.findOneBy({ id });
+		let result;
+		if(userToRemove)
+			result = await userRepository.remove(userToRemove);
+	
+		return response.json(result);
+	} catch (error) {
+		return response.status(500).json({
+			message: 'An error has occurred'
+		});
+	}
 };
 
