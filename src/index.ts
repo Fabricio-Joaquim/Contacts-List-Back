663c8844b26082a3adfd63070fc7aca497ce452b
Fabricio Joaquim <fabriciojoaquim25@hotmail.com>
2022-04-21 22:32:11 -0300

    mudanca de porta e Procfile

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {AppDataSource} from './connection';
 
 async function main() {
 	await AppDataSource.initialize();
-	app.listen(3000, () => {
+	app.listen(process.env.PORT ||3000, () => {
 		console.log('Server is running on port 3000');
 	});
 }
